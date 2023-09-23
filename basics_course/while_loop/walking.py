d61e723443ec949bb8618f802ce8033cbf0868a0
emodk <emil_dk@yahoo.com>
2023-09-23 12:22:38 +0300

    This is a homework from basics codding in Phyton - title "While loop".

---
+++ basics_course/while_loop/walking.py
@@ -0,0 +1,21 @@
+day_target = 10000
+while current_steps != 'Going home':
+    steps = int(current_steps)
+    total_day_steps_counter += steps
+    if total_day_steps_counter >= day_target:
+        diff = total_day_steps_counter - day_target
+        print('Goal reached! Good job!')
+        print(f'{diff} steps over the goal!')
+        break
+    current_steps = input()
+if total_day_steps_counter < day_target:
+    current_steps = input()
+    steps = int(current_steps)
+    total_day_steps_counter += steps
+    if total_day_steps_counter >= day_target:
+        diff = total_day_steps_counter - day_target
+        print('Goal reached! Good job!')
+        print(f'{diff} steps over the goal!')
+    else:
+        diff_less = day_target - total_day_steps_counter
+        print(f'{diff_less} more steps to reach goal.')
