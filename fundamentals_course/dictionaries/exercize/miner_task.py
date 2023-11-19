df5f1ccd67d9b902bee202439d815bf420eda193
emodk <emil_dk@yahoo.com>
2023-11-19 22:47:11 +0200

    miner_task.py updated

--- fundamentals_course/dictionaries/exercize/miner_task.py
+++ fundamentals_course/dictionaries/exercize/miner_task.py
@@ -1,14 +1,14 @@
 resource_dict = {}
-while True:
-    key, value = input(), input()
-    if key == "stop" or value == "stop":
+a = input()
+while a != "stop":
+    b = input()
+    if b == "stop":
         break
+    b = int(b)
+    if a not in resource_dict.keys():
+        resource_dict[a] = b
     else:
-        value = int(value)
-        if key not in resource_dict.keys():
-            resource_dict[key] = value
-        else:
-            resource_dict[key] += value
-# print(resource_dict)
+        resource_dict[a] += b
+    a = input()
 for (key, value) in resource_dict.items():
     print(f'{key} -> {value}')
