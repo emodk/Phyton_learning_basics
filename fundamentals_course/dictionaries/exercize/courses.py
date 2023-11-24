7c80f3de1a7333ce8db1f15683f83b5acd75c80b
emodk <emil_dk@yahoo.com>
2023-11-24 07:50:32 +0200

    uploading homework file: courses.py

---
+++ fundamentals_course/dictionaries/exercize/courses.py
@@ -0,0 +1,15 @@
+courses = {}
+course_info = input()
+while course_info != "end":
+    course_details = course_info.split(":")
+    course_name = course_details[0]
+    student_name = course_details[1]
+    if course_name not in courses:
+        courses[course_name] = [student_name]
+    else:
+        courses[course_name].append(student_name)
+    course_info = input()
+for key in courses:
+    print(f'{key}: {len(courses[key])}')
+    for i in range(len(courses[key])):
+        print(f"-- {courses[key][i]}")
